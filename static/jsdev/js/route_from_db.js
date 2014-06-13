2e192619ee79a920a0b03b826b633ad6a5d31a24
demid5111 <monadv@yandex.ru>
2014-06-13 16:37:59 +0400

    Refactored code. Added functionality for displaying routes. Some extra files deleted.

--- static/jsdev/js/route_from_db.js
+++ static/jsdev/js/route_from_db.js
@@ -12,45 +12,57 @@ var map;
     map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
 	directionsDisplay = new google.maps.DirectionsRenderer();
-	 directionsDisplay.setMap(map);
+	directionsDisplay.setMap(map);
 	
-  
-	//moved to html in order to render
-	//var json_obj = {{ obj_as_json|safe }};
 	
 	waypt = [];
 	var start, end;
-	//marker1.setPosition(json_obj['lat0'], json_obj['lon0']);
+
 	alert("Got " + Object.keys(json_obj).length + " points");
 	if (Object.keys(json_obj).length==0) return;
-	//alert(Object.keys(json_obj));
-	//alert(json_obj["point_1"]["lat"]);
-	for (var i=0; i<Object.keys(json_obj).length; i++ ) {
-		if (i==0)  
-			start = new google.maps.LatLng(json_obj["point_"+i]["lat"], json_obj["point_"+i]["lon"], true);
-		else if (i==Object.keys(json_obj).length-1)
-			end = new google.maps.LatLng(json_obj["point_"+i]["lat"], json_obj["point_"+i]["lon"], true);
-		else
+
+	pointsId = Object.keys(json_obj)
+	
+	alert("Got " + json_obj[pointsId[0]]["lat"] + " points");
+
+	for (var i=0; i < pointsId.length;i++) {
+		alert ("I: " + i)
+		if (i == 0) {
+			start = new google.maps.LatLng(json_obj[pointsId[i]]["lat"], json_obj[pointsId[i]]["lon"], true);
+			alert ("Start " + start + i)
+			console.log(start)
+		}
+		else if (i == pointsId.length -1)
+		{
+			end = new google.maps.LatLng(json_obj[pointsId[i]]["lat"], 	json_obj[pointsId[i]]["lon"], true);
+			alert ("End " + end + i)
+		}
+		else {
 			waypt.push({
-				location: new google.maps.LatLng(json_obj["point_"+i]["lat"], json_obj["point_"+i]["lon"], true),
+				location: new google.maps.LatLng(json_obj[pointsId[i]]["lat"], json_obj[pointsId[i]]["lon"], true),
 				stopover: true
 			});
+		}
 	}
-
+	console.log(start)
+	console.log(end)
 	
-	//waypt[0] = new google.maps.LatLng(json_obj["lat0"], json_obj["lon0"], true);
-	//waypt[1] = new google.maps.LatLng(json_obj["lat1"], json_obj["lon1"], true);
-	
+	//this is how to manipulate with google maps
+	//we should construct such a request
 	var request = {
     origin: start,
     destination: end,
 	waypoints: waypt,
     travelMode: google.maps.TravelMode.WALKING
   };
-  
+  console.log(request)
   directionsService.route(request, function(result, status) {
+  	
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(result);
+    }
+    if (status == google.maps.DirectionsStatus.ZERO_RESULT) {
+      alert("not able to build route")
     }
   });
  
