e80d9ac90a9c83be7a9a22b79bbe38e79f9c8a33
yarik-tarkan <yarik_tarkan@mail.ru>
2014-09-10 18:28:14 +0400

    Revert "UI design with django static sources"
    This reverts commit 45b07deeb5b3ca25e10b91f9ee25f2fe66f35d8a.

--- static/jsdev/js/city_locator.js
+++ static/jsdev/js/city_locator.js
@@ -39,97 +39,6 @@ function geo_error(err) {
     });
   //printAddress(geoip_latitude(), geoip_longitude(), true);
 }
-//Custom API for Google Reverse Geocoding
-function getLocation(){
-    var loc = document.getElementById('coords');
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else
-    {
-        loc.value = "nolocation";
-    }
-}
-//Custom API for Google Reverse Geocoding
-function showPosition(position){
-    var loc = document.getElementById('coords');
-    loc.value = position.coords.latitude + "," + position.coords.longitude;
-
-    var geocoder = new google.maps.Geocoder();
-     var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if (results[0]) {
-
-
-                var area = getArea(results)
-                document.getElementById('location-box').value = area;
-            }
-        }
-    });
-}
-// Get ONLY COUNTRY from Google Reverse Geocoding
-function getCountry(results)
-{
-    for (var i = 0; i < results[0].address_components.length; i++)
-    {
-        var shortname = results[0].address_components[i].short_name;
-        var longname = results[0].address_components[i].long_name;
-        var type = results[0].address_components[i].types;
-        if (type.indexOf("country") != -1)
-        {
-            if (!isNullOrWhitespace(longname))
-            {
-                return longname;
-            }
-            else
-            {
-                return shortname;
-            }
-        }
-    }
-    return "";
-}
-// Get ONLY CITY from Google Reverse Geocoding
-function getArea(results)
-{
-    var areaName = "";
-    for (var i = 0; i < results[0].address_components.length; i++)
-    {
-        var shortname = results[0].address_components[i].short_name;
-        var longname = results[0].address_components[i].long_name;
-        var type = results[0].address_components[i].types;
-
-        if (type.indexOf("locality") != -1){
-            if (!isNullOrWhitespace(shortname))
-            {
-                areaName += shortname;
-            }
-            else
-            {
-                areaName += longname;
-            }
-        }
-        if (type.indexOf("administrative_area_level_2") != -1 && areaName == ""){
-            if (!isNullOrWhitespace(shortname))
-            {
-                areaName += shortname;
-            }
-            else
-            {
-                areaName += longname;
-            }
-        }
-    }
-    return areaName;
-}
-function isNullOrWhitespace(text) {
-    if (text == null) {
-        return true;
-    }
-    return text.replace(/\s/gi, '').length < 1;
-}
  
 // use Google Maps API to reverse geocode our location
 function printAddress(latitude, longitude) {
@@ -144,7 +53,7 @@ function printAddress(latitude, longitude) {
     if(status == google.maps.GeocoderStatus.OK) {
       if(results[0]) {
         $("#city_name").fadeOut(function() {
-          $(this).text(getArea(results)).fadeIn();
+          $(this).text(results[0].formatted_address).fadeIn();
         })
       } else {
         error("Google did not return any results.");
