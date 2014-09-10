28ea68957d333a663c837e388e1f7e042a38a348
yarik-tarkan <yarik_tarkan@mail.ru>
2014-09-10 18:27:05 +0400

    GUI path changes

---
+++ static/jsdev/js/addRoute.js
@@ -0,0 +1,30 @@
+function addRoute() {
+    var route_name = document.getElementById('route_name').value;
+    var route_likes = document.getElementById('route_likes').value
+    var route_city = document.getElementById('route_city').value
+    var route_length = document.getElementById('route_length').value
+    var route_duration = document.getElementById('route_duration').value
+
+    alert(route_name + ", " + route_likes + ", " + route_city + ", " + route_length + ", " + route_duration);
+    
+    var route = {};
+    route["route_name"] = route_name;
+    route["route_likes"] = route_likes;
+    route["route_city"] = route_city;
+    route["route_length"] = route_length;
+    route["route_duration"] = route_duration;
+
+    jsonString = JSON.stringify(route);
+    
+    $.ajax ({
+			url: "/addRoute/",
+			type: "POST",
+			data: jsonString,
+			success : function(data) {
+				alert(data);
+				},
+			
+		});
+
+
+}
