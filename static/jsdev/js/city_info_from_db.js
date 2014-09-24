11a6c1ec1d19dccd3f01f9d2a0375abb6c14ba29
mafatekhov <brutalduck@gmail.com>
2014-09-24 11:30:19 +0400

    list of cities filtered

--- static/jsdev/js/city_info_from_db.js
+++ static/jsdev/js/city_info_from_db.js
@@ -1,13 +1,32 @@
-
+// appends recieved info from Cities db to list
 $(document).ready (function(){
-alert('ok');
-var $select = $('#mySelectID');
+
+
 
 for (var i in Object.keys(json_obj)){
 
-
-var $option = $("<option/>").attr("value", json_obj[i]["city_id"]).text(json_obj[i]["city_name"]);
-$select.append($option);
+  
+  var list = document.getElementById("list");
+  var li = document.createElement("li");
+  var liValue = document.createTextNode(json_obj[i]["city_name"]);
+  li.appendChild(liValue);
+  list.appendChild(li);
 
 }
+
+
+
 });
+// filters list onkeyup
+function filter(element) {
+        var value = $(element).val();
+
+        $("#list > li").each(function() {
+            if ($(this).text().search(value) > -1) {
+                $(this).show();
+            }
+            else {
+                $(this).hide();
+            }
+        });
+    }
